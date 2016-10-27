08a3b8a6bc3d7360f3a84b9fd0bdd48632b375b4
MDu <marioldurao@gmail.com>
2016-10-27 09:37:45 +0100

    First Commit 1. Turns On Bluetooth 2. Discovery of Bluetooth devices 3. Connect to Deviced 4. Sending Messages by text (Debug on Salea Logic) 5. Have seek bar and label with seekbar value when released 6. send of seekbar values (current and temp) when released on byte array;
    next steps:
    a)verify big/litle endian of firmware for data;
    b)ajust message size concerning data or not data
    c) use enums to cmds
    
    All project commit

--- Application/src/main/java/com/abs/mdu/bluetoothchat/MainActivity.java
+++ Application/src/main/java/com/abs/mdu/bluetoothchat/MainActivity.java
@@ -20,6 +20,7 @@ package com.abs.mdu.bluetoothchat;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Xml;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -37,6 +38,12 @@ import com.abs.mdu.common.logger.LogFragment;
 import com.abs.mdu.common.logger.LogWrapper;
 import com.abs.mdu.common.logger.MessageOnlyLogFilter;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
 /**
  * A simple launcher activity containing a summary sample description, sample log and a custom
  * {@link android.support.v4.app.Fragment} which can display a view.
@@ -48,9 +55,19 @@ public class MainActivity extends SampleActivityBase {
 
     public static final String TAG = "MainActivity";
     private SeekBar seekBar;
+    private SeekBar seekBar2;
     private TextView textView;
+    private TextView textView2;
     private EditText mOutEditText;
     private Button mSendButton;
+    private int id;
+    byte  start_byte;
+    byte  setCurrent;
+    byte  getCurrent;
+    byte  setTemperature;
+    byte  getTemperature;
+    byte  opc;
+    byte  status;
 
     // Whether the Log Fragment is currently shown
     private boolean mLogShown;
@@ -72,7 +89,33 @@ public class MainActivity extends SampleActivityBase {
         initializeVariables();
 
         // Initialize the textview with '0'.
-        textView.setText("Covered: " + seekBar.getProgress());
+        textView.setText("Current: " + seekBar.getProgress());
+        textView2.setText("Temperature: " + seekBar.getProgress());
+
+        seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textView2.setText("Temperature: " + progress );
+
+                try {
+                    sendMsg(progress, setTemperature);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progress = 0;
@@ -90,9 +133,12 @@ public class MainActivity extends SampleActivityBase {
             public void onStopTrackingTouch(SeekBar seekBar) {
                         textView.setText("Current: " + progress );
                         mOutEditText = (EditText) findViewById(R.id.edit_text_out);
-                        mOutEditText.setText("Current: " + progress );
-                        mSendButton = (Button) findViewById(R.id.button_send);
-                        mSendButton.callOnClick();
+                try {
+                    sendMsg(progress, setCurrent);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
         });
 
@@ -153,8 +199,66 @@ public class MainActivity extends SampleActivityBase {
     // A private method to help us initialize our variables.
     private void initializeVariables() {
         seekBar = (SeekBar) findViewById(R.id.seekBar1);
+        seekBar2 = (SeekBar) findViewById(R.id.seekBar2);
         textView = (TextView) findViewById(R.id.textView1);
-        seekBar.setMax(500);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        seekBar.setMax(600);
+        seekBar2.setMax(70);
+        id = 0;
+        start_byte =    0x7E;
+        setCurrent =    0x0A;
+        getCurrent =    0x0B;
+        setTemperature = 0x1A;
+        getTemperature = 0x1B;
+        opc =           0x08;
+        status =        0x00;
     }
+
+    private void sendMsg(int progress, byte cmd) throws IOException {
+
+        mOutEditText = (EditText) findViewById(R.id.edit_text_out);
+
+
+
+        byte[] temperature_msg = new byte[11];
+        Arrays.fill( temperature_msg, (byte) 0 );
+
+        temperature_msg[0] = start_byte;
+        //length in the end
+        temperature_msg[2] = opc;
+        temperature_msg[3] = (byte) id;
+        temperature_msg[4] = cmd;
+        temperature_msg[5] = status;
+
+        ByteBuffer buffer = ByteBuffer.allocate(4);
+//b.order(ByteOrder.BIG_ENDIAN); // optional, the initial order of a byte buffer is always BIG_ENDIAN.
+        buffer.putInt(progress);
+
+        byte[] byte_progess = buffer.array();
+        temperature_msg[6] =  byte_progess [0];
+        temperature_msg[7] =  byte_progess [1];
+        temperature_msg[8] =  byte_progess [2];
+        temperature_msg[9] =  byte_progess [3];
+        temperature_msg[1] = (byte) temperature_msg.length;
+
+        int crc, i;
+        crc=0;
+        for (i=0; i< temperature_msg.length; i++) {
+            crc |= temperature_msg[i];
+        }
+
+        temperature_msg[10] = (byte) crc;
+        //length must be in the end, when msg is completely build;
+
+        //String temperature_msg_string = Arrays.toString(temperature_msg);
+        String temperature_msg_string = new String(temperature_msg);
+
+        id++;
+        mOutEditText.setText(temperature_msg_string);
+        mSendButton = (Button) findViewById(R.id.button_send);
+        mSendButton.callOnClick();
+    }
+
+
 
 }
